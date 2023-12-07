5816d36bed130c74e4b153c67cbbdcd7cb5bc6ec
EAngel-pro <69425159+EAngel-pro@users.noreply.github.com>
2023-12-07 00:36:38 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//This is a simple Java class that can be executed from the Command Prompt by entering the commands below.
+//javac HelloWorld.java
+//java HelloWorld
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
